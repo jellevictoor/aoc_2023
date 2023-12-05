9870dadf082b3d62a7515969e9a9cbfb3877324c
Jelle <jelle@victoor.io>
2023-12-05 21:04:11 +0100

    rest day 5 passes

--- day5.py
+++ day5.py
@@ -1,9 +1,57 @@
+import re
+from dataclasses import dataclass
+
 from functions import clean_lines
 
 
-def solve(input):
-    pass
+@dataclass
+class Seed:
+    seed_id: int
 
+
+@dataclass
+class SeedMap:
+    name: str
+    destination: [int]
+    source: [int]
+
+    def get_destination(self, id:int):
+        if id in self.source:
+            return self.destination[self.source.index(id)]
+        else:
+            return id
+
+
+def solve(lines):
+    seeds = [Seed(int(id)) for id in re.findall(r'(\d+)', lines[0])]
+    name = ""
+    seed_maps = []
+    destination = list()
+    source = list()
+    for line in lines[1:]:
+        split = line.split(' ')
+        if re.match(r'.*\smap:', line):
+            if name:
+                seed_maps.append(SeedMap(name, destination, source))
+                name = ''
+                source = []
+                destination = []
+            name = split[0]
+        if re.match(r'\d* \d* \d*', line):
+            dest_start = int(split[0])
+            source_start = int(split[1])
+            length = int(split[2])
+            destination.extend(range(dest_start, dest_start+length))
+            source.extend(range(source_start, source_start+length))
+    seed_maps.append(SeedMap(name, destination, source))
+
+    result = []
+    for seed in seeds:
+        next = seed.seed_id
+        for seed_map in seed_maps:
+            next = seed_map.get_destination(next)
+        result.append(next)
+    return min(result)
 
 def main():
     with open('input/day5.txt', 'r') as f:
@@ -11,7 +59,40 @@ def main():
 
 
 def test_day5_star1():
-    input = """"""
-    expected = -1
+    input = """seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+"""
+    expected = 35
     actual = solve(clean_lines(input))
     assert expected == actual
