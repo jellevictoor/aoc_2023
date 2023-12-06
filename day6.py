e3a53db9350c673370076de08736f4ffc4064f10
Jelle <jelle@victoor.io>
2023-12-06 08:08:30 +0100

    parsing done

--- day6.py
+++ day6.py
@@ -1,8 +1,13 @@
+import re
 
 from functions import clean_lines
 
+
 def solve(lines):
-    ...
+    time = [int(time) for time in re.findall(r'\d+', lines[0])]
+    distance = [int(distance) for distance in re.findall(r'\d+', lines[1])]
+    for index in range(len(time)):
+        print(f"race {index+1}, time: {time[index]}, distance: {distance[index]}")
 
 
 def main():
@@ -11,8 +16,10 @@ def main():
 
 
 def test_day6_star1():
-    input = """\
+    input = """
+Time:      7  15   30
+Distance:  9  40  200
 """
-    expected = 35
+    expected = 288
     actual = solve(clean_lines(input))
     assert actual == expected
