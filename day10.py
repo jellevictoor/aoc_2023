e8c9107fb6bb8982de5772d812d6f5a1131dc0bf
Jelle <jelle@victoor.io>
2023-12-10 19:53:01 +0100

    keep steps

--- day10.py
+++ day10.py
@@ -4,12 +4,10 @@ from typing import List
 from functions import clean_lines
 
 WEST_VALUES = ('S', '-', 'L', 'F')
-
-SOUTH_VALUES = ('S', '|', 'L', 'J')
-
 EAST_VALUES = ('S', '-', '7', 'J')
 
 NORTH_VALUES = ('S', '|', 'F', '7')
+SOUTH_VALUES = ('S', '|', 'L', 'J')
 
 
 @dataclass
@@ -47,8 +45,8 @@ class Map:
         starting_position = self.get_starting_position()
         direction = ''
         current_position = starting_position
-        steps = 0
-        while current_position != starting_position or steps == 0:
+        steps = []
+        while current_position != starting_position or len(steps) == 0:
             neighbours = self.get_neighbours(current_position)
             # | is a vertical pipe connecting north and south.
             # - is a horizontal pipe connecting east and west.
@@ -56,22 +54,21 @@ class Map:
             # J is a 90-degree bend connecting north and west.
             # 7 is a 90-degree bend connecting south and west.
             # F is a 90-degree bend connecting south and east.
-
             if direction != 'SOUTH' and neighbours[0].value in NORTH_VALUES:
                 direction = 'NORTH'
                 current_position = self.get_position(current_position.row - 1, current_position.col)
-            elif direction != 'WEST' and neighbours[1].value in EAST_VALUES:
-                direction = 'EAST'
-                current_position = self.get_position(current_position.row, current_position.col + 1)
             elif direction != 'NORTH' and neighbours[2].value in SOUTH_VALUES:
                 direction = 'SOUTH'
                 current_position = self.get_position(current_position.row + 1, current_position.col)
             elif direction != 'EAST' and neighbours[3].value in WEST_VALUES:
                 direction = 'WEST'
                 current_position = self.get_position(current_position.row, current_position.col - 1)
+            elif direction != 'WEST' and neighbours[1].value in EAST_VALUES:
+                direction = 'EAST'
+                current_position = self.get_position(current_position.row, current_position.col + 1)
             else:
-                raise Exception('stuck')
-            steps += 1
+                raise Exception(f'stuck on {current_position}, coming from {direction} in {steps} steps.')
+            steps.append(current_position)
         return steps
 
     def get_position(self, row, col):
@@ -79,7 +76,7 @@ class Map:
 
 
 def solve(lines):
-    return Map(lines).get_steps() / 2
+    return len(Map(lines).get_steps()) / 2
 
 
 def main():
